b66a6998d3bc14a7511da8d859b2358951f5a098
Bogdan <bogdanvoskanov@gmail.com>
2024-05-23 18:51:00 +0300

    task_3 done

--- Source.cpp
+++ Source.cpp
@@ -1,126 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-#include <thread>
-#include <time.h>
-#include <chrono>
-#include <vector>
+#include <string.h>
+#include <mutex>
 
+#define MAX_FILES 1000
+#define MAX_FILE_NAME 32
+#define MAX_DATA_SIZE 1024
 
-void fillMatrix(int** matrix, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        matrix[i] = new int[cols];
-        for (int j = 0; j < cols; ++j) {
-            std::cout << "Enter value for matrix[" << i << "][" << j << "]: ";
-            std::cin >> matrix[i][j];
-        }
-    }
-}
+using namespace std;
 
-void mulMat(int** mat1, int rows1, int cols1, int** mat2, int rows2, int cols2, int** result) {
-
-    for (int i = 0; i < rows1; ++i) {
-        for (int j = 0; j < cols2; ++j) {
-            result[i][j] = 0;
-            for (int k = 0; k < cols1; ++k) {
-                result[i][j] += mat1[i][k] * mat2[k][j];
-            }
-        }
-    }
-    
-}
-
-void mulMatTh(int** mat1, int rows1, int cols1, int** mat2, int rows2, int cols2, int row, int** result)
+typedef struct File
 {
+    char name[MAX_FILE_NAME];
+    char data[MAX_DATA_SIZE];
+    int size;
+    bool isModifying;
+    mutex mtx;
+} File;
 
-    for (int i = 0; i < rows1; i++) {
-        result[row][i] = 0;
-        for (int j = 0; j < cols2; j++) {
-            result[row][i] += mat1[row][j] * mat2[j][i];
+File files[MAX_FILES];
+int cur_file_idx = 0;
+
+void createFile(const char* name_)
+{
+    if (cur_file_idx < MAX_FILES) {
+        strcpy_s(files[cur_file_idx].name, name_);
+        cout << "File '" << name_ << "' created successfully." << endl;
+        cur_file_idx++;
+    }
+    else {
+        cout << "Cannot create file '" << name_ << "'. Maximum number of files reached." << endl;
+    }
+}
+
+int readFile(const char* name_)
+{
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (strcmp(files[i].name, name_) == 0) {
+            lock_guard<mutex> lock(files[i].mtx);
+
+            cout << "File content for '" << name_ << "': " << files[i].data << endl;
+
+            return 0;
         }
     }
+
+    cout << "File with the name '" << name_ << "' not found." << endl;
+    return -1;
+}
+
+int writeFile(const char* name_, const char* text_)
+{
+    for (int i = 0; i < cur_file_idx; i++)
+    {
+        if (strcmp(files[i].name, name_) == 0)
+        {
+            lock_guard<mutex> lock(files[i].mtx);
+
+            memset(files[i].data, '\0', sizeof(files[i].data));
+
+            strcpy_s(files[i].data, text_);
+
+            files[i].size = strlen(text_);
+
+            cout << "File '" << name_ << "' has been updated successfully." << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int deleteFile(const char* name_) {
+    int fileIndex = -1;
+
+    for (int i = 0; i < cur_file_idx; i++) {
+        if (strcmp(files[i].name, name_) == 0) {
+            fileIndex = i;
+            break;
+        }
+    }
+
+    if (fileIndex == -1) {
+        cout << "No file with the name '" << name_ << "' found.\n";
+        return -1;
+    }
+
+    if (files[fileIndex].isModifying) {
+        cout << "File is being modified and cannot be deleted.\n";
+        return -1;
+    }
+
+    for (int i = fileIndex; i < cur_file_idx; i++) {
+        strcpy_s(files[i].name, files[i + 1].name);
+        strcpy_s(files[i].data, files[i + 1].data);
+        files[i].size = files[i + 1].size;
+    }
+
+    cur_file_idx--;
+
+    std::cout << "File '" << name_ << "' has been deleted successfully.\n";
+
+    return 0;
+}
+
+void fsInfo()
+{
+    int totalFiles = 0;
+    int totalSize = 0;
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        if (files[i].name[0] != '\0')
+        {
+            totalFiles++;
+            totalSize += files[i].size;
+        }
+    }
+    cout << "Total files: " << totalFiles << endl << "Total size bytes: " << totalSize << endl;
 }
 
 int main()
 {
-    int rows1, cols1, rows2, cols2;
-
-    std::cout << "Enter the number of rows and columns for matrix 1: ";
-    std::cin >> rows1 >> cols1;
-
-    std::cout << "Enter the number of rows and columns for matrix 2: ";
-    std::cin >> rows2 >> cols2;
-
-    int** mat1 = new int* [rows1];
-    int** mat2 = new int* [rows2];
-
-    std::cout << "Enter values for matrix 1:" << std::endl;
-    fillMatrix(mat1, rows1, cols1);
-
-    std::cout << "Enter values for matrix 2:" << std::endl;
-    fillMatrix(mat2, rows2, cols2);
-
-
-    int** result1 = new int* [rows1];
-    for (int i = 0; i < rows1; ++i) {
-        result1[i] = new int[cols2];
-    }
-
-    int** result2 = new int* [rows1];
-    for (int i = 0; i < rows1; ++i) {
-        result2[i] = new int[cols2];
-    }
-
-    
-    std::vector<std::thread> threads;
-
-    auto start1 = std::chrono::system_clock::now();
-
-    for (int i = 0; i < rows1; ++i) {
-        threads.emplace_back([i, mat1, rows1, cols1, mat2, rows2, cols2, &result1]() {
-            mulMatTh(mat1, rows1, cols1, mat2, rows2, cols2, i, result1);
-            });
-    }
-
-    for (auto& t : threads) {
-        t.join();
-    }
-
-    std::cout << "Result of matrix multiplication:" << std::endl;
-    for (int i = 0; i < rows1; ++i) {
-        for (int j = 0; j < cols2; ++j) {
-            std::cout << result1[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Free memory for result matrix
-    for (int i = 0; i < rows1; ++i) {
-        delete[] result1[i];
-    }
-    delete[] result1;
-
-    auto end1 = std::chrono::system_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count() << "ms with multi" << std::endl;
-
-
-
-    auto start2 = std::chrono::system_clock::now();
-    mulMat(mat1, rows1, cols1, mat2, rows2, cols2, result2);
-    auto end2 = std::chrono::system_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count() << "ms without multi" << std::endl;
-
-    std::cout << "Result of matrix multiplication:" << std::endl;
-    for (int i = 0; i < rows1; ++i) {
-        for (int j = 0; j < cols2; ++j) {
-            std::cout << result2[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Free memory for result matrix
-    for (int i = 0; i < rows1; ++i) {
-        delete[] result2[i];
-    }
-    delete[] result2;
-
     return 0;
 }
